2b1324e651868e1cebdf76ebae5ae5e31eef1249
aldoutm <agins498@gmail.com>
2022-03-26 15:54:03 +0700

    add validations

--- Tugas2.java
+++ Tugas2.java
@@ -1,15 +1,103 @@
 import java.util.Scanner;
 public class Tugas2 {
+
+    static int scanInt(Scanner scan, int min, int max) {
+        String in;
+        int result = 0;
+        boolean isValid = false;
+
+        do {
+            in = scan.nextLine();
+
+            isValid = true;
+
+            try { 
+                result = Integer.parseInt(in); 
+            }  
+            catch (NumberFormatException e) { 
+                System.out.println("Input tidak valid! Harus berupa angka. (Ctrl+C untuk keluar)");
+                isValid = false;
+            }
+
+            if (isValid && (result < min || result > max)) {
+                System.out.printf("Input tidak valid! Minimal %d , maksimal %d. (Ctrl+C untuk keluar)\n", min, max);
+                isValid = false;
+            }
+
+            if (!isValid) {
+                System.out.print("Coba lagi: ");
+            }
+
+        } while (!isValid);
+
+        return result;
+    }
+
+    static boolean promptRetry(Scanner scan) {
+        boolean result = false;
+        boolean isValid = false;
+        do {
+            //Tanya user untuk mengulangi perhitungan
+            String exit_prog;
+            System.out.print("\n Anda ingin mengulang [y/t] : ");
+            exit_prog = scan.nextLine();
+
+            if (exit_prog.equals("Y") || exit_prog.equals("y")) {
+                result = true;
+                isValid = true;
+            }
+
+            if (exit_prog.equals("T") || exit_prog.equals("t")) {
+                result = false;
+                isValid = true;
+            }
+        } while(!isValid);
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String exit_prog;
-        //        Input user data
-//        Pesanan dan nama
-        System.out.print("\nMasukkan Nama Anda [1..25] : ");
-        String nama_mhs = scan.nextLine();
+        boolean shouldRetry = false;
 
-        System.out.print("Masukkan NIM Anda [harus 10 karakter]: ");
-        int nim_mhs = Integer.parseInt(scan.nextLine());
+        // Input user data
+        // nama
+        String nama_mhs;
+        boolean isNameValid = false;
+        do {
+            System.out.print("\nMasukkan Nama Anda [1..25 karakter] : ");
+            nama_mhs = scan.nextLine();
+
+            isNameValid = nama_mhs.length() > 0 && nama_mhs.length() <=25;
+
+            if (!isNameValid) {
+                System.out.println("Nama tidak valid! Nama harus 1..25 karakter. (Ctrl+C untuk keluar)");
+            }
+        } while (!isNameValid);
+
+        // NIM
+        String nim_mhs;
+        boolean isNIMValid = false;
+        do {
+            System.out.print("\nMasukkan NIM Anda [10 karakter] : ");
+            nim_mhs = scan.nextLine();
+
+            isNIMValid = true;
+            if (nim_mhs.length() != 10) {
+                System.out.println("NIM tidak valid! Nama harus 10 karakter. (Ctrl+C untuk keluar)");
+                isNIMValid = false;
+            }
+
+            try 
+            { 
+                Integer.parseInt(nim_mhs); 
+            }  
+            catch (NumberFormatException e)  
+            { 
+                System.out.println("NIM tidak valid! NIM berupa angka. (Ctrl+C untuk keluar)");
+                isNIMValid = false;
+            }
+
+        } while (!isNIMValid);
 
         System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 //        Nama menu
@@ -20,12 +108,13 @@ public class Tugas2 {
 //        var num for store variable input
 
         do {
-            int num;
             System.out.print("\n Masukkan Sembarang Angka [5..20] : ");
-            num = Integer.parseInt(scan.nextLine());
+            int num = scanInt(scan, 5, 20);
+
             System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
             System.out.println("\nDeret Bilangan" + "\n##############");
             System.out.print("\n " + num + " Bilangan Genap : \n");
+            
             //Genap
             int sumGenap = 0;
             for (int i = 1; i <= num * 2; i++) {
@@ -35,6 +124,7 @@ public class Tugas2 {
                 }
             }
             System.out.println(" \nHasil Bilangan Genap = " + sumGenap);
+
             //Ganjil
             System.out.print("\n " + num + " Bilangan Ganjil : \n");
             int sumGanjil = 0;
@@ -45,6 +135,7 @@ public class Tugas2 {
                 }
             }
             System.out.println(" \nHasil Bilangan Ganjil = " + sumGanjil);
+
             //Fibanocci
             System.out.print("\n " + num + " Bilangan Fibonacci : \n");
             int sum = 0;
@@ -59,14 +150,11 @@ public class Tugas2 {
                 n2 = sum;
             }
             int n3 = sum + n1 + n2 - 1; //-1 Starts with 1 not zero
-            System.out.print("\nHasil Bilangan Fibonacci = " + n3);
+            System.out.print("\nHasil Bilangan Fibonacci = " + n3 + "\n");
 
-            //Tanya user untuk mengulangi perhitungan
-            System.out.print("\n Anda ingin mengulang [y/t] : ");
-            exit_prog = scan.nextLine();
+            shouldRetry = promptRetry(scan);
 
-
-        }while (!exit_prog.equals("T") && !exit_prog.equals("t"));
+        } while (shouldRetry);
     }
 
 }
